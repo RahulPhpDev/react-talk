fd3412f154a4cf361ff32e9b235a5a342bac4e5b
maxrahul7 <rahul.chauhan7@maxlifeinsurance.com>
2024-10-19 23:42:47 +0530

    solution

--- src/components/UserReducer/Todos.jsx
+++ src/components/UserReducer/Todos.jsx
@@ -4,25 +4,60 @@ import Button from '../common/Button';
 import Checkbox from '../common/Checkbox';
 import EmailRef from '../common/EmailRef';
 
+function reducerFunction(state, action) {
+    switch(action.type) {
+        case 'CHANGE_TITLE': {
+            return {
+                ...state,
+                title: 'Add List'
+            }
+        }
+        case 'ADD_TO_DO':
+            return {...state,
+               todos: [
+                ...state?.todos,
+                    {title: action?.title,
+                    isComplete:false}
+                ]
+            };
+        case 'MARK_COMPLETED': {
+            console.log(state)
+             state.todos[action?.index].isComplete = !state?.todos[action?.index].isComplete;
+             return {...state}
+        }
+        default:
+            return []
+        
+    }
+}
  const Todos = (props) => {
-    const [todos, setTodos] = React.useState([]);
+    const [state, dispatch] = React.useReducer(reducerFunction , {todos: [] , title: 'Add Todos'} )
+    // const [title, setTitle] = React.useState('Add Todos')
+    // const [todos, setTodos] = React.useState([]);
     const inputRef = React.useRef('');
     const addTodos = () => {
-        setTodos([...todos, {id: todos?.length, title:inputRef?.current?.value, isComplete:false}]); 
+         dispatch({type: 'ADD_TO_DO',title: inputRef?.current?.value })
+        // setTodos([...todos, {id: todos?.length, title:inputRef?.current?.value, isComplete:false}]); 
         inputRef.current.value = '';
     }
     const markAction = (index) => {
-        console.log({index})
-       todos[index].isComplete = !todos[index].isComplete;
-       setTodos([...todos]);
+        dispatch({type: 'MARK_COMPLETED',index: index })
+        // console.log({index})
+    //    todos[index].isComplete = !todos[index].isComplete;
+    //    setTodos([...todos]);
     }
     return (
         <>
+        <div className='flex mb-2'>
+       <h1 className='text-xl font-bold mr-5'> {state?.title} </h1>
+       <Button clickHandler ={() => dispatch({type: 'CHANGE_TITLE'})}> Change Title</Button>
+       </div>
         <div className='flex'>
+            
             <EmailRef type = "text" ref = {inputRef} classNames="w-3/4" placeholder="Enter your Todos" />
             <Button classNames="ml-2" clickHandler={addTodos}> Add Todo</Button>
         </div>
-            <ShowToDos todos={todos} markAction={markAction} />
+            <ShowToDos todos={state?.todos} markAction={markAction} />
         </>
     );
 };
